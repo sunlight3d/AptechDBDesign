22ee793f256324e9fa993d7715218fa69c642746
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-13 15:01:43 +0700

    update

--- c1808i-01/database.sql
+++ c1808i-01/database.sql
@@ -1,10 +1,53 @@
 CREATE DATABASE dbMerciado;
+USE dbMerciado;
 CREATE TABLE IF NOT EXISTS tblUsers(
-    userName VARCHAR(300),
+    name VARCHAR(300),
     email VARCHAR(300),
     password_hash VARCHAR(400),
     phone VARCHAR(500) NOT NULL PRIMARY KEY,
-    role VARCHAR(100), --user,"admin",
-    dob DATE,
+    role VARCHAR(100), 
+    dob DATE
 );
-
+--role = user,"admin",
+CREATE TABLE IF NOT EXISTS tblRestaurants(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(300) NOT NULL,
+    description TEXT,
+    address TEXT,
+    phone VARCHAR(100),
+    imageURL VARCHAR(100)
+);
+CREATE TABLE IF NOT EXISTS tblEntertainments(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(300) NOT NULL,
+    description TEXT,
+    address TEXT,
+    phone VARCHAR(100),
+    imageURL VARCHAR(100)
+);
+CREATE TABLE IF NOT EXISTS tblGallerys(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(300) NOT NULL,
+    description TEXT,
+    address TEXT,
+    phone VARCHAR(100),
+    imageURL VARCHAR(100)
+);
+--Loai ve
+CREATE TABLE IF NOT EXISTS tblTicketTypes(
+	id VARCHAR(300) NOT NULL PRIMARY KEY,
+    name VARCHAR(300) NOT NULL,
+    description TEXT,
+    price FLOAT
+);
+USE dbMerciado;
+CREATE TABLE IF NOT EXISTS tblOrders(
+	id VARCHAR(400),
+	userPhone VARCHAR(500) NOT NULL,
+	FOREIGN KEY (userPhone) REFERENCES tblUsers(phone),
+	ticketTypeId VARCHAR(300) NOT NULL,
+	FOREIGN KEY (ticketTypeId) REFERENCES tblTicketTypes(id),
+	description TEXT,
+	datetime DATETIME,
+	numberOfTickets INTEGER DEFAULT 1
+);
