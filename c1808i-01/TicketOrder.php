77b7eca77d8e4901f6416069c65246cb35fcd07b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-18 15:52:48 +0700

    update

--- c1808i-01/TicketOrder.php
+++ c1808i-01/TicketOrder.php
@@ -9,11 +9,54 @@ class TicketOrder {
 		if(!mysqli_error()) {
 			echo json_encode(array(
 				"result" => "ok", 
-				"sql" => $sql,
+				"data" => $result,
 				"message"=>"Insert data successfully"
 			));
 		} else {
 			echo json_encode(array("result" => "failed", "message"=>"Insert data failed"));
+		}
+	}
+	public function updateTicketOrder($orderId, 
+		$userPhone,$ticketTypeId, 
+		$description,$numberOfTickets, $active) {
+
+		$sql = "UPDATE tblOrders SET userPhone='$userPhone',".
+				"ticketTypeId='$ticketTypeId'".
+				",description='$description'".
+				",numberOfTickets='$numberOfTickets'".
+				" WHERE id='$orderId'";
+		$database = new Database();
+		$connection = $database->get_connection();
+		$result = mysqli_query($connection, $sql);
+		if(!mysqli_error()) {
+			echo json_encode(array(
+				"result" => "ok", 
+				"data" => $sql,
+				"message"=>"Update data successfully"
+			));
+		} else {
+			echo json_encode(array("result" => "failed", "message"=>"Update data failed"));
+		}
+	}
+	public function queryTicketOrders($limit, $page) {
+		$offset = $limit * $page;
+		$sql = "SELECT * FROM tblOrders LIMIT $limit OFFSET $offset";
+		$database = new Database();
+		$connection = $database->get_connection();
+		$result = mysqli_query($connection, $sql);
+		$ticketOrders = array();
+		while($row = mysqli_fetch_assoc($result)) {
+			array_push($ticketOrders, $row);
+		}
+
+		if(!mysqli_error()) {
+			echo json_encode(array(
+				"result" => "ok", 
+				"data" => $ticketOrders,
+				"message"=>"Query data successfully"
+			));
+		} else {
+			echo json_encode(array("result" => "failed", "message"=>"Query data failed"));
 		}
 	}
 }
--- c1808i-01/ticketOrders.php
+++ c1808i-01/ticketOrders.php
@@ -6,14 +6,30 @@ header("Content-Type: application/json; charset=UTF-8");
 
 //http://localhost:81/c1808i-01/ticketOrders.php
 include "./TicketOrder.php";
-if(isset($_POST)) {
-	echo "<h1>hello </h1>";
+if(isset($_POST) && count($_POST) > 0) {
+	
 	$userPhone = $_POST["userPhone"];
 	$ticketTypeId = $_POST["ticketTypeId"];
 	$description = $_POST["description"];
 	$numberOfTickets = $_POST["numberOfTickets"];
-
+	$active = $_POST["active"];
 	$ticketOrder = new TicketOrder();
-	$ticketOrder->insertTicketOrder($userPhone,$ticketTypeId, $description,$numberOfTickets);
+
+	if(isset($_POST["orderId"])) {
+		//update
+			$orderId = $_POST["orderId"];
+			$ticketOrder->updateTicketOrder($orderId, 
+			$userPhone,$ticketTypeId, 
+			$description,$numberOfTickets, $active);
+	} else {
+		//insert	
+		$ticketOrder->insertTicketOrder($userPhone,$ticketTypeId, $description,$numberOfTickets);
+	}
+} else if(isset($_GET) && count($_GET) > 0){
+	echo "GET";
+	$ticketOrder = new TicketOrder();
+	$page = $_GET["page"];
+	$limit = $_GET["limit"];
+	$ticketOrder->queryTicketOrders($limit, $page);
 }
 ?>
